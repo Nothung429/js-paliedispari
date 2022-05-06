fa16e1d967d3745bcd7d39ced2fb70959a111176
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-06 17:38:47 +0200

    correzione js palindromo

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -1,10 +1,20 @@
 // Chiedere all’utente di inserire una parola
-const wordRequest = prompt("Inserisci una parola ed io verificherò se è palindroma ;)")
-console.log(wordRequest);
-// Creare una funzione per capire se la parola inserita è palindroma
-const letterArray = [];
-letterArray.push(wordRequest);
-console.log(letterArray);
-for (let i = 0 ; i < letterArray.length ; i++) {
-    alert(letterArray.length);
+const word = prompt("Inserisci una parola ed io verificherò se è palindroma ;)")
+console.log(word);
+let wordReverse = "";
+// parola lettera per lettera left to right
+for (let i = 0 ; i < word.length ; i++) {
+    console.log(i , word[i]);
 }
+// parola lettera per lettera right to left
+for (let i = word.length - 1 ; i >= 0 ; i--) {
+    console.log(i , word[i]);
+    wordReverse += word[i];
+}
+// Adesso fare il confronto tra la parola e il suo contrario e vedere se sono uguali o diverse
+if (word === wordReverse) {
+    alert("La parola è palindroma");
+} else {
+    alert("La parola non è palindroma");
+}
+// Se sono uguali è palindroma, se sono diverse non lo è
