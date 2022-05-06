7fbb8e20b2cbc7de39a17348b35ed9ae97f63331
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-06 14:34:34 +0200

    aggiunta commenti e js per palindromo

---
+++ Palindromo/js/script.js
@@ -0,0 +1,7 @@
+// Chiedere all’utente di inserire una parola
+const wordRequest = prompt("Inserisci una parola ed io verificherò se è palindroma ;)")
+console.log(wordRequest);
+// Creare una funzione per capire se la parola inserita è palindroma
+const letterArray = [];
+letterArray.push(wordRequest);
+console.log(letterArray);
