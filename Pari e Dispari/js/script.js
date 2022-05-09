0dd211c38ff1c4286eac0de967cd4a395efa956c
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-09 12:15:34 +0200

    correzione e aggiunta controlli di input

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,17 +1,28 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const oddEven = prompt("Scegli tra pari e dispari")
+// L’utente sceglie tra pari o dispari
+let oddEven;
+do {
+    oddEven = prompt("Scegli tra pari e dispari")
+} while (oddEven !== "pari" && oddEven !== "dispari");
 console.log(oddEven);
-const playerNumber = Number(prompt("Scegli un numero tra 1 e 5"));
+
+// L'utente sceglie un numero tra 1 e 5
+let playerNumber;
+do {
+    playerNumber = Number(prompt("Scegli un numero tra 1 e 5"));
+} while (isNaN(playerNumber) || playerNumber < 1 || playerNumber > 5);
 console.log(playerNumber);
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 function getRandomNumber (min , max) {
     return Math.floor(Math.random() * (max - min +1)) + min;
 }
 const cpuNumber = getRandomNumber(1,5);
 console.log(cpuNumber);
+
 // Sommiamo i due numeri
 const sum = cpuNumber + playerNumber;
 console.log(sum);
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 let sumEven = 0;
 let sumOdd = 0;
@@ -20,6 +31,7 @@ if ( sum % 2 === 0 ) {
 } else {
     sumOdd = "dispari";
 }
+
 // Dichiariamo chi ha vinto
 if (oddEven === sumEven || oddEven === sumOdd) {
     alert("hai vinto");
